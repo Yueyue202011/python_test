559a4924f2bc7948a7a3c56527d9d163f8989262
yuequan <1498194198@qq.com>
2020-11-20 10:20:48 +0800

    第一次向GitHub提交代码

---
+++ demo.py
@@ -0,0 +1,28 @@
+print("hello world") 
+ 
+
+# a = float(input("请输入:"))
+# b = float(input("请输入:"))
+# print("input获取的内容：",a+b)
+
+
+# print(type("大师傅似的"))
+# print(type(2233))
+# print(type(1.66))
+# print(type([]))
+# print(type(False))
+# print(type(()))
+# print(type({})) 
+
+# a = 22
+# print(float(a))
+
+
+# a = "fsadasd啊实打实"
+# print(len(a))
+
+a = len(input("请输入内容："))
+b = len(input("请输入内容："))
+c = len(input("请输入内容："))
+d = len(input("请输入内容："))
+print("总长度：",a+b+c+d)
